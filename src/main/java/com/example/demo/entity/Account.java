dce807f7434e2e1ff1cb0e3c1515e0e737ad1730
manumura <emmanuel.mura@gmail.com>
2022-06-30 23:43:20 +0700

    modify db schema

--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -2,7 +2,10 @@ package com.example.demo.entity;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.LocalDateTime;
 
 
 @Builder
@@ -12,4 +15,16 @@ public class Account {
     @Id
     private Long id;
     private String name;
+    @CreatedBy
+    @Column("created_by")
+    private String createdBy;
+    @CreatedDate
+    @Column("created_date")
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    @Column("last_modified_by")
+    private String lastModifiedBy;
+    @LastModifiedDate
+    @Column("last_modified_date")
+    private LocalDateTime lastModifiedDate;
 }
